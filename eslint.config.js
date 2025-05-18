664c237974621684224e8ebe458d6c10c8064a7e
danielbonkowsky <danielbonkowsky@users.noreply.github.com>
2025-05-18 01:16:51 +0000

    Prettified Code!

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,22 @@
 import js from "@eslint/js";
 import globals from "globals";
-import stylistic from '@stylistic/eslint-plugin';
+import stylistic from "@stylistic/eslint-plugin";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs}"],
     languageOptions: {
-      globals: globals.browser
+      globals: globals.browser,
     },
     plugins: {
       js,
-      "@stylistic": stylistic
+      "@stylistic": stylistic,
     },
     extends: ["js/recommended"],
     rules: {
       // require curly braces for all control statements (if, else, for, while, etc.)
-      curly: "error", 
+      curly: "error",
 
       // always require semicolons at the end of statements
       "@stylistic/semi": ["error", "always"],
@@ -25,10 +25,10 @@ export default defineConfig([
       "@stylistic/brace-style": "error",
 
       // enforce consistent indentation (2 spaces, with switch cases indented once)
-      "@stylistic/indent": ["error", 2, { "SwitchCase": 1 }],
+      "@stylistic/indent": ["error", 2, { SwitchCase: 1 }],
 
       // limit the number of statements per line to improve readability
-      "@stylistic/max-statements-per-line": ["error", { "max": 1 }]
-    }
-  }
+      "@stylistic/max-statements-per-line": ["error", { max: 1 }],
+    },
+  },
 ]);
