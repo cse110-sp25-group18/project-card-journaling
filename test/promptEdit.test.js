d59c3c95676d5b1cb73b5aecaf04252f207c974e
Sentixxx <mouxinc@gmail.com>
2025-05-20 17:52:51 -0700

    style: fix eslint warning (#33)

--- test/promptEdit.test.js
+++ test/promptEdit.test.js
@@ -1,4 +1,5 @@
-const { makePromptEditable } = require("../script/promptEdit.js");
+import { makePromptEditable } from "../script/promptEdit.js";
+import { test, expect } from "@jest/globals";
 
 test("makePromptEditable doesn't return a value", () => {
   const result = makePromptEditable();
