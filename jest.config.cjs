d1c3c6ed9b220d6de8e06a3645eb84a9dede343a
danielbonkowsky <danielbonkowsky@gmail.com>
2025-05-23 17:03:41 -0700

    ci: add e2e testing (#63)
    * feat: add puppeteer requirements to package/package-lock.json
    
    * feat: added unit and e2e sections to jest config
    
    * refactor: renamed unit tests to align with jest config
    
    * feat: added puppeteer config file
    
    * feat: initial e2e test for home page
    
    * refactor: moved unit tests into dedicated folder
    
    * refactor: moved e2e tests into dedicated folder
    
    * feat: initialized e2e testing for other pages
    
    * refactor: updated imports to match new unit test locations
    
    * fix: added missing async to test
    
    * ci: removed unnecessary puppeteer config file
    
    * feat: improved card detection on create card page
    
    * ci: removed unused test file
    
    * feat: added new scripts for testing
    
    * fix: wait for card to load before testing begins
    
    * feat: initialized e2e github actions workflow
    
    * fix: typo in e2e.yml
    
    * fix: working on create card test bug
    
    * fix: testing waiting for card to load
    
    * feat: add prompt styles and motivational text to settings (#56)
    
    * fix: temporarily removed card detection from createcard e2e
    
    * fix: attempt to fix bug using waitForSelector
    
    * refactor: changed e2e action to run on pull requests to main
    
    * feat: added comments to jest config explaining test setup
    
    * Prettified Code!
    
    * fix: more waitForSelectors to fix race condition
    
    * refactor: changed to only activate on prs to main
    
    * Prettified Code!
    
    * fix: added missing toBes
    
    * fix: added missing await
    
    * fix: added global header to fix ESLint warnings
    
    ---------
    
    Co-authored-by: ryansoe <125227157+ryansoe@users.noreply.github.com>
    Co-authored-by: danielbonkowsky <danielbonkowsky@users.noreply.github.com>

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,3 +1,19 @@
 module.exports = {
-  testEnvironment: "jsdom",
+  verbose: true,
+  maxWorkers: 1,
+  projects: [
+    // configuration for unit tests
+    {
+      displayName: "unit",
+      testEnvironment: "jsdom",
+      testMatch: ["**/test/**/*.unit.test.js"],
+    },
+
+    // configuration for end-to-end tests 
+    {
+      displayName: "e2e",
+      preset: "jest-puppeteer",
+      testMatch: ["**/test/**/*.e2e.test.js"],
+    },
+  ]
 };
