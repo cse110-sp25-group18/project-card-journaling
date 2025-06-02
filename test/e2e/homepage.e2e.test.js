3e6b81e1c3dfd345794ba7411460da7c08b62868
domwrm <domonickwm14@gmail.com>
2025-06-01 19:22:28 -0700

    fix: updated homepage e2e test

--- test/e2e/homepage.e2e.test.js
+++ test/e2e/homepage.e2e.test.js
@@ -31,8 +31,8 @@ describe("Test basic user flow from homepage", () => {
     });
 
     expect(homeButtons.length).toBe(2);
-    expect(homeButtons.includes("📝 Create a new journal entry?")).toBe(true);
-    expect(homeButtons.includes("📔 View past journal entries?")).toBe(true);
+    expect(homeButtons.includes("Create a new journal entry?")).toBe(true);
+    expect(homeButtons.includes("View past journal entries?")).toBe(true);
   });
 
   it.skip(`Testing 'new journal entry' button`, async () => {
