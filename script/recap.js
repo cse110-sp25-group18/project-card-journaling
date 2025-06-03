101d68103328a326a480681396bafa069e3b4e53
juicebox013 <justin.nguyen335@gmail.com>
2025-06-03 02:12:37 -0700

    feat: changed indent to match ESLint

--- script/recap.js
+++ script/recap.js
@@ -1,13 +1,5 @@
-document
-  .getElementById("shuffle-button")
-  .addEventListener("click", function () {
-    document.getElementById("placeholderImage").style.display = "none";
-    document.getElementById("cardContent").style.display = "block";
+document.getElementById('shuffle-button').addEventListener('click', function () {
+  document.getElementById('placeholderImage').style.display = 'none';
+  document.getElementById('cardContent').style.display = 'block';
+});
 
-    // // Show the flippable card
-    // document.getElementById('cardContent').style.display = 'block';
-
-    // // Optional: Populate with new data if needed
-    // document.getElementById('cardPrompt').textContent = "New Prompt Goes Here";
-    // document.getElementById('response').textContent = "New Response Goes Here";
-  });
