27bfd759c728875f6ba2a84db11de6960eeea180
domwrm <165512399+domwrm@users.noreply.github.com>
2025-06-03 00:44:11 -0700

    feat(homepage): add css styling (#105)
    * Changed home-page button styles
    
    * fix: made home page css responsive
    
    * fix: updated homepage e2e test
    
    * fix: added generated PR changes
    
    * fix: updaed e2e test
    
    ---------
    
    Co-authored-by: sentixxx <1091441107@qq.com>

--- test/e2e/homepage.e2e.test.js
+++ test/e2e/homepage.e2e.test.js
@@ -24,15 +24,15 @@ describe("Test basic user flow from homepage", () => {
 
     console.log("Checking that journal entry buttons loaded...");
 
-    const homeButtons = await page.$$eval(".home-buttons button", (buttons) => {
+    const homeButtons = await page.$$eval(".home-buttons", (buttons) => {
       return buttons.map((button) => {
         return button.textContent.trim();
       });
     });
 
     expect(homeButtons.length).toBe(2);
-    expect(homeButtons.includes("📝 Create a new journal entry?")).toBe(true);
-    expect(homeButtons.includes("📔 View past journal entries?")).toBe(true);
+    expect(homeButtons.includes("Create a new journal entry?")).toBe(true);
+    expect(homeButtons.includes("View past journal entries?")).toBe(true);
   });
 
   it.skip(`Testing 'new journal entry' button`, async () => {
