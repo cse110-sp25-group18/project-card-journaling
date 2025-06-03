fd2521052d243d41dbd8cd759aba3a8098232887
juicebox013 <justin.nguyen335@gmail.com>
2025-06-03 02:02:54 -0700

    feat: resovled merge conflicts for recap page and added some functionality to shuffle button

---
+++ script/recap.js
@@ -0,0 +1,12 @@
+document.getElementById('shuffle-button').addEventListener('click', function () {
+    document.getElementById('placeholderImage').style.display = 'none';
+    document.getElementById('cardContent').style.display = 'block';
+
+    // // Show the flippable card
+    // document.getElementById('cardContent').style.display = 'block';
+
+    // // Optional: Populate with new data if needed
+    // document.getElementById('cardPrompt').textContent = "New Prompt Goes Here";
+    // document.getElementById('response').textContent = "New Response Goes Here";
+});
+
