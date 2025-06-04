27c0ed9f417edbf37a55f2f120c6cf5be8195b8e
Justin Nguyen <122576230+juicebox013@users.noreply.github.com>
2025-06-03 18:03:07 -0700

    feat: add some functionality to shuffle button (#115)
    * feat: resovled merge conflicts for recap page and added some functionality to shuffle button
    
    * style: prettified code with prettier
    
    commit by github action
    
    * feat: changed indent to match ESLint
    
    * style: prettified code with prettier
    
    commit by github action
    
    ---------
    
    Co-authored-by: juicebox013 <juicebox013@users.noreply.github.com>

---
+++ script/recap.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("shuffle-button")
+  .addEventListener("click", function () {
+    document.getElementById("placeholderImage").style.display = "none";
+    document.getElementById("cardContent").style.display = "block";
+  });
